0adc6756ec7c6f8351998e7a220687666cc52f83
AswanthGPillai <aswanthgpillai@gmail.com>
2025-10-27 00:13:31 +0530

    msg

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace <REPOSITORY-NAME> with the actual name of your GitHub repository.
-// For example, if your repo is github.com/username/my-rente-app, use '/my-rente-app/'
-const REPO_NAME = '/<REPOSITORY-NAME>/'; 
+
 
 export default defineConfig({
   // Set the base path for deployment to a subfolder (like GitHub Pages)
-  base: REPO_NAME, 
+  base: "/your-repo-name", // Replace 'your-repo-name' with your actual repository name
   plugins: [react()],
 });
